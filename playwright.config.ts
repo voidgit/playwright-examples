8e4c71619141f910d8903374595d4521f6de141f
voidgit <none>
2025-02-22 15:16:07 +0100

    merging reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
 // Playwright will automatically set the number of workers to the number of logical CPU cores available on the system.
 // This ensures optimal parallelism without manual configuration.
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
